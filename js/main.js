0b3006a8eccce8f79f4326ff380ae1f7c658410e
Alec Jones <alecjones@Alecs-MacBook-Air.local>
2021-12-10 22:36:27 -0600

    create css-card-deck, and initial js HTML and CSS

---
+++ js/main.js
@@ -0,0 +1,58 @@
+/*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+// Build a 'master' deck of 'card' objects used to create shuffled decks
+const masterDeck = buildMasterDeck();
+
+/* state variables */
+let shuffledDeck, dealersHand, playersHand = [];
+let playersCardScore, dealersCardScore;
+
+/* cached elements */
+const playerContainer = document.getElementById('player-hand-container');
+
+/* event listeners */
+let hitBtnEl = document.getElementById('hit-btn')
+    .addEventListener('click', playerHit);
+let stayBtnEl = document.getElementById('stay-btn')
+    .addEventListener('click', playerStay);
+
+
+/* functions */
+function buildMasterDeck() {
+    const deck = [];
+    // Use nested forEach to generate card objects
+    suits.forEach(function (suit) {
+        ranks.forEach(function (rank) {
+            deck.push({
+                // The 'face' property maps to the library's CSS classes for cards
+                face: `${suit}${rank}`,
+                // Setting the 'value' property for game of blackjack, not war
+                value: Number(rank) || (rank === 'A' ? 11 : 10)
+            });
+        });
+    });
+    return deck;
+}
+
+function getNewShuffledDeck() {
+    // Create a copy of the masterDeck (leave masterDeck untouched!)
+    const tempDeck = [...masterDeck];
+    const newShuffledDeck = [];
+    while (tempDeck.length) {
+        // Get a random index for a card still in the tempDeck
+        const rndIdx = Math.floor(Math.random() * tempDeck.length);
+        // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+        newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    return newShuffledDeck;
+}
+
+function playerHit() {
+    console.log('hit button');
+}
+
+function playerStay() {
+    console.log('stay button');
+}
